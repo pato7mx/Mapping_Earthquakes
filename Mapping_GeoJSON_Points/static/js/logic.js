7348a14d51582fc0bcf55b3580b735c0de0cde6e
pato7mx <patriciotamayo17@gmail.com>
2022-03-07 18:20:49 -0600

    Add files

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,62 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.5, -122.5], 10);  // San Francisco Airport
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grab GeoJSON data // ---------------
+//L.geoJSON(sanFranAirport).addTo(map);   
+
+// Grab GeoJSON data and Add popupmarkers:
+// 1. With pointToLayer ------------
+//L.geoJSON(sanFranAirport, {
+//    // We turn each feature into a marker on the map.
+//    pointToLayer: function(feature, latlng) {
+//      console.log(feature);
+//      return L.marker(latlng)
+//      .bindPopup("<h2>" + feature.properties.city + "</h2>");
+//    }
+//  }).addTo(map);
+
+// 2. With onEachFeature ------------
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+  }).addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    //id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
